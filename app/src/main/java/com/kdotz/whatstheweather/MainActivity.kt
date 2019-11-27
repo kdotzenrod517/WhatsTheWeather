ca3f4d4ac939c4e24886e387ebbebbd1a498dd8c
Krista Dotzenrod <kdotzenrod517@gmail.com>
2019-11-26 22:26:13 -0600

    Adding in temp and hiding keyboard

--- app/src/main/java/com/kdotz/whatstheweather/MainActivity.kt
+++ app/src/main/java/com/kdotz/whatstheweather/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.kdotz.whatstheweather
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.TextView
 import com.kdotz.whatstheweather.repository.SearchRepositoryProvider
@@ -16,6 +18,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var city: EditText
     private lateinit var description: TextView
+    private lateinit var temp: TextView
 
     private val compositeDisposable: CompositeDisposable = CompositeDisposable()
 
@@ -26,11 +29,13 @@ class MainActivity : AppCompatActivity() {
 
         city = findViewById(R.id.editText)
         description = findViewById(R.id.description)
+        temp = findViewById(R.id.temp)
 
         loadCurrentWeather("Minneapolis")
     }
 
     private fun loadCurrentWeather(city : String) {
+        val res = resources
         val repository = SearchRepositoryProvider.provideSearchRepository()
         compositeDisposable.add(
             repository.getCurrentWeather(city)
@@ -38,6 +43,7 @@ class MainActivity : AppCompatActivity() {
                 .subscribeOn(Schedulers.io())
                 .subscribe({ result ->
                     description.text = result.weather.first().description.toUpperCase()
+                    temp.text = String.format(res.getString(R.string.temp), result.main.temp)
                     Log.d("Result", "There current weather in Minneapolis is a high of ${result.main.temp} with ${result.weather.first().description}")
                 }, { error ->
                     error.printStackTrace()
@@ -45,8 +51,14 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
+    private fun View.hideKeyboard() {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(windowToken, 0)
+    }
+
     fun onClick(view: View) {
         loadCurrentWeather(city.text.toString())
+        view.hideKeyboard()
     }
 
     override fun onDestroy() {
